883754a23900a51abf8cc86c8f005d7484d8b78a
Harin <harindesrawala8126@gmail.com>
2026-09-18 13:53:37 +0530

    models fixed maybe

--- r_models/kmeans.R
+++ r_models/kmeans.R
@@ -1,23 +1,17 @@
-#K-MEANS CLUSTERING
-# 1. Create the dataset
+
 data <- read.csv("data/data.csv")
 
-# print(data)
 dframe <- data.frame(data)
 data_cluster <- dframe[, c("AnnualIncome", "SpendingScore")]
 
-# 2. Set seed and run K-means
 set.seed(123)
 kmeans_model <- kmeans(data_cluster, centers = 3)
 
-# 3. Print model details and centroids
 print(kmeans_model)
 print(kmeans_model$centers)
 
-# 4. Save cluster assignments (Fixed the lowercase 'c' typo here)
 dframe$Cluster <- kmeans_model$cluster
 
-# 5. Plot the data points colored by cluster
 plot(
   dframe$AnnualIncome, dframe$SpendingScore,
   col = dframe$Cluster,
@@ -27,7 +21,6 @@ plot(
   ylab = "Spending Score"
 )
 
-# 6. Add the centroid stars to the plot
 points(
   kmeans_model$centers[,"AnnualIncome"],
   kmeans_model$centers[,"SpendingScore"],
